522f8302e0db071de5f77d94ffb13ec7372ec2fa
yeseulbae <qodptmf96@gmail.com>
2019-08-12 23:23:03 +0900

    190812-2

---
+++ yeseulbae/190812-2.py
@@ -0,0 +1,21 @@
+# 구구단
+
+for i in range(1, 9 + 1):
+    print(2, '*', 1, "=", 2 * 1)
+
+
+def multiplyy(x, y):
+    return f'{x} * {y} = {x * y}'
+
+
+def test_multiplication_table():
+    #초기값
+    multiplications = []
+    # 누산
+    for i in range(1, 9 + 1)
+        multiplications.append(multiplyy(2 , i)
+    return multiplications
+
+def test_multiply():
+    assert multiplyy(2, 1) == '2 * 1 = 2'
+    assert multiplyy(3, 6) == '3 * 6 = 1'
